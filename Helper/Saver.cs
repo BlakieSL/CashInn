d6909e64a08940465bf152c6363c3773354c28c8
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-10-22 22:34:49 +0200

    serialization + deserialization

---
+++ Helper/Saver.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace CashInn.Helper;
+
+public class Saver
+{
+    public void Serialize<T>(T obj, string filePath)
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        string jsonString = JsonSerializer.Serialize(obj, options);
+        File.WriteAllText(filePath, jsonString);
+    }
+    
+    public T? Deserialize<T>(string filePath)
+    {
+        string jsonString = File.ReadAllText(filePath);
+        
+        T? obj = JsonSerializer.Deserialize<T>(jsonString);
+
+        if (obj == null)
+        {
+            throw new InvalidOperationException("Error deserialization");
+        }
+        
+        return obj;
+    }
+}
